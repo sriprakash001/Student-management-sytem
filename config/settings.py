81e174b00432c47d1712c218ec20e14a09cedd1b
demo <email>
2026-04-07 13:44:36 +0530

    small changes

--- config/settings.py
+++ config/settings.py
@@ -184,3 +184,14 @@ SPECTACULAR_SETTINGS = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.up.railway.app",
 ]
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
